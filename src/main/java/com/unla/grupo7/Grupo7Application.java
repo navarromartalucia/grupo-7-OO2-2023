043294c38fb69e36efabb608c502bf33fccc2fce
luschwind <navarromartalucia@gmail.com>
2023-05-21 21:12:02 -0300

    PROYECTO BASE

---
+++ src/main/java/com/unla/grupo7/Grupo7Application.java
@@ -0,0 +1,13 @@
+package com.unla.grupo7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo7Application.class, args);
+	}
+
+}
